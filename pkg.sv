93a4ccf9fcb59d7e6d4ec4161dc84217f7782763
Neha Y B <nehabangera@mirafra.com>
2025-08-22 14:18:01 +0530

    Create pkg.sv

---
+++ pkg.sv
@@ -0,0 +1,16 @@
+package my_pkg;
+    `include "uvm_macros.svh"
+    import uvm_pkg::*;
+
+    `include "defines.sv"
+    `include "seq_item.sv"
+    `include "my_sequence.sv"
+    `include "my_sequencer.sv"
+    `include "my_driver.sv"
+    `include "my_monitor.sv"
+    `include "my_agent.sv"
+    `include "my_scoreboard.sv"
+    `include "my_coverage.sv"
+    `include "my_env.sv"
+    `include "my_test.sv"
+endpackage
